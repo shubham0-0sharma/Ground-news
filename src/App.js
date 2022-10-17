991434f7caf6063f16f7977b01035b35a2af3a8a
shubham sharma <shubham00sharma0@gmail.com>
2022-10-18 01:44:43 +0530

    added top loading bar and env local

--- src/App.js
+++ src/App.js
@@ -4,17 +4,33 @@ import React, { Component } from "react";
 import NavBar from "./components/NavBar";
 import News from "./components/News";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import LoadingBar from 'react-top-loading-bar'
+
+
 
 export default class App extends Component {
   pageSize = 10
+  state = {
+    progress : 0
+  }
+  setProgress = (progress) => {
+this.setState({
+progress:progress})
+  }
+
     render() {
         return (
             <div>
                 <Router>
+                <LoadingBar
+        color='#f11946'
+        progress={this.state.progress}
+        height = { 3}
+      />
                     <NavBar />
                     <Switch>
                         <Route exact path="/">
-                            <News key = "general"  
+                            <News  setProgress = {this.setProgress} key = "general"  
                                 category="general"
                                 pageSize={this.pageSize} 
                                 country="in"
@@ -22,41 +38,41 @@ export default class App extends Component {
                         </Route>
 
                         <Route exact path="/business">
-                            <News key = "business"
+                            <News  setProgress = {this.setProgress}key = "business"
                                 category="business"
                                 pageSize={this.pageSize} 
                                 country="in"
                             />
                         </Route>
                         <Route exact path="/general">
-                            <News key = "general"
+                            <News  setProgress = {this.setProgress}key = "general"
                                 category="general"
                                 pageSize={this.pageSize} 
                                 country="in"
                             />
                         </Route>
                         <Route exact path="/entertainment">
-                            <News key = "entertainmen"
+                            <News  setProgress = {this.setProgress}key = "entertainmen"
                                 category="entertainment"
                                 pageSize={this.pageSize} 
                                 country="in"
                             />
                         </Route>
                         <Route path="/health">
-                            <News key = "science" category="health" pageSize={this.pageSize}  country="in" />
+                            <News  setProgress = {this.setProgress}key = "science" category="health" pageSize={this.pageSize}  country="in" />
                         </Route>
                         <Route path="/science">
-                            <News key = ""
+                            <News  setProgress = {this.setProgress}key = ""
                                 category="science"
                                 pageSize={this.pageSize} 
                                 country="in"
                             />
                         </Route>
                         <Route exact path="/sports">
-                            <News key = "sports" category="sports" pageSize={this.pageSize}  country="in" />
+                            <News  setProgress = {this.setProgress}key = "sports" category="sports" pageSize={this.pageSize}  country="in" />
                         </Route>
                         <Route exact path="/technology">
-                            <News key = "technology"
+                            <News  setProgress = {this.setProgress}key = "technology"
                                 category="technology"
                                 pageSize={this.pageSize} 
                                 country="in"
--- src/components/News.js
+++ src/components/News.js
@@ -23,15 +23,19 @@ export class News extends Component {
     }
 
     async update() {
+        this.props.setProgress(10)
         let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=4fd60328d07f407db32ab5532e866c63&page=${this.state.page}&pageSize=${this.props.pageSize}`;
         this.setState({ loading: true });
         let data = await fetch(url);
+        this.props.setProgress(30)
         let parsedData = await data.json();
+        this.props.setProgress(70)
         this.setState({
             articles: parsedData.articles,
             totalResults: parsedData.totalResults,
             loading: false,
         });
+        this.props.setProgress(100)
     }
 
     async componentDidMount() {
