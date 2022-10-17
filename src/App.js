60f08a17a9cabf15d7b886e86f15da63945126f8
shubham sharma <shubham00sharma0@gmail.com>
2022-10-17 19:11:46 +0530

    Included React Router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,69 @@
- import './App.css';
+import "./App.css";
 
-import React, { Component } from 'react'
-import NavBar from './components/NavBar';
-import News from './components/News';
-
+import React, { Component } from "react";
+import NavBar from "./components/NavBar";
+import News from "./components/News";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <Router>
+                    <NavBar />
+                    <Switch>
+                        <Route exact path="/">
+                            <News key = "general"  
+                                category="general"
+                                pageSize={8}
+                                country="in"
+                            />
+                        </Route>
 
-  render() {
-    return (
-      <div>
-        <NavBar/>
-        <News pageSize = {5}/>
-       
-      </div>
-    )
-  }
+                        <Route exact path="/business">
+                            <News key = "business"
+                                category="business"
+                                pageSize={8}
+                                country="in"
+                            />
+                        </Route>
+                        <Route exact path="/general">
+                            <News key = "general"
+                                category="general"
+                                pageSize={8}
+                                country="in"
+                            />
+                        </Route>
+                        <Route exact path="/entertainment">
+                            <News key = "entertainmen"
+                                category="entertainment"
+                                pageSize={8}
+                                country="in"
+                            />
+                        </Route>
+                        <Route path="/health">
+                            <News key = "science" category="health" pageSize={8} country="in" />
+                        </Route>
+                        <Route path="/science">
+                            <News key = ""
+                                category="science"
+                                pageSize={8}
+                                country="in"
+                            />
+                        </Route>
+                        <Route exact path="/sports">
+                            <News key = "sports" category="sports" pageSize={8} country="in" />
+                        </Route>
+                        <Route exact path="/technology">
+                            <News key = "technology"
+                                category="technology"
+                                pageSize={8}
+                                country="in"
+                            />
+                        </Route>
+                    </Switch>
+                </Router>
+            </div>
+        );
+    }
 }
--- src/components/News.js
+++ src/components/News.js
@@ -1,8 +1,18 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
-
+import PropTypes from 'prop-types';
 export class News extends Component {
+
+    static defaultProps = {
+        category : 'general',
+        country : "in",
+        pageSize : 8,
+    }
+    static propsTypes = {
+        country : PropTypes.string,
+        pageSize : PropTypes.number
+    }
     constructor() {
         super();
         this.state = {
@@ -13,7 +23,7 @@ export class News extends Component {
     }
 
     async componentDidMount() {
-        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=4fd60328d07f407db32ab5532e866c63&page=1&pageSize=${this.props.pageSize}`;
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=4fd60328d07f407db32ab5532e866c63&page=1&pageSize=${this.props.pageSize}`;
         this.setState({loading:true})
         let data = await fetch(url);
         let parsedData = await data.json();
@@ -26,7 +36,7 @@ export class News extends Component {
 
     handlePrevClick = async () => {
         console.log("Previous");
-        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=4fd60328d07f407db32ab5532e866c63&page=${
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&apiKey=4fd60328d07f407db32ab5532e866c63&page=${
             this.state.page - 1
         }&pageSize=${this.props.pageSize}`;
         this.setState({loading:true})
@@ -46,7 +56,7 @@ export class News extends Component {
             !(this.state.page + 1 >
             Math.ceil(this.state.totalResults / this.props.pageSize)
         )) {
-            let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=4fd60328d07f407db32ab5532e866c63&page=${
+            let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&apiKey=4fd60328d07f407db32ab5532e866c63&page=${
                 this.state.page + 1
             }&pageSize=${this.props.pageSize}`;
             this.setState({loading:true})
@@ -59,11 +69,13 @@ export class News extends Component {
             });
         }
     };
-
+    styles = {
+        margin : "35px 0px",
+    }
     render() {
         return (
             <div className="container my-3">
-                <h1 className="text-center">My News App - Top Headlines</h1>
+                <h1 className="text-center" style={this.styles}>My News App - Top Headlines</h1>
                 {(this.state.loading) && <Spinner />}
                 <div className="row">
                     {!(this.state.loading) && this.state.articles.map((element) => {
